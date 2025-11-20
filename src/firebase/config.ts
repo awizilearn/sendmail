4f4b299f320049b1f3d8ee4e74a67a42a7588495
Admin WIzi Learn <awizilearn@gmail.com>
2025-11-20 11:56:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2406795398-63e69",
+  "appId": "1:125879483014:web:5c3eb576c5fb4e5cc5af07",
+  "apiKey": "AIzaSyBIO5d2ZRRLi3qHZ8bHi9Gh4DECmfRa4s0",
+  "authDomain": "studio-2406795398-63e69.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "125879483014"
+};
